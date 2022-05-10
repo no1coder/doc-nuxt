93abd431d544c99fe205189b4d7835e2d428142e
liuwenfei <1436635489@qq.com>
2022-05-10 10:35:13 +0800

    lwf:5.10早上

---
+++ components/personal.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="w-screen h-screen bg-black pt-8">
+    <div class="container h-4/5 m-auto">
+        <div class="  bg-white block-inline float-left h-full w-1/5    xl:w-full  xl:h-1/5 xl:mb-10">
+            
+        </div>
+
+        <div class=" bg-white block-inline float-right h-full w-[78%]   xl:w-full  xl:h-4/5   ">
+            
+        </div>
+        
+    </div>
+</div>
+ 
+</template>
+
+<script lang="ts" setup>
+    
+</script>
+
+<style>
+    
+</style>
