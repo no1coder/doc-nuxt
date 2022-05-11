355081208ba8181941cac5f3e73f29debd67a71c
lin <lin@xue.com>
2022-05-11 10:00:54 +0800

    0511 xll

--- components/Document/fixicon/Mobile.vue
+++ components/Document/fixicon/Mobile.vue
@@ -13,16 +13,10 @@ export default {
 </script>
 
 <style scoped>
+@import "assets/css/mycss.css";
 img{
   width:20px;
   height:20px;
 }
-.back{
-  width:42px;
-  height:42px;
-  border:1px solid red;
-  border-radius:42px;
-  background:#ffffff;
-  box-shadow: 0 1px 4px -2px rgb(0 0 0 / 13%), 0 2px 8px rgb(0 0 0 / 8%), 0 8px 16px 4px rgb(0 0 0 / 4%);
-}
+
 </style>
--- components/Document/fixicon/Zan.vue
+++ components/Document/fixicon/Zan.vue
@@ -14,21 +14,11 @@ export default {
 </script>
 
 <style scoped>
+@import "assets/css/mycss.css";
+
 img{
   width:20px;
   height:20px;
 }
-.back{
-  width:42px;
-  height:42px;
-  border-radius:42px;
-  background:#ffffff;
-  box-shadow: 0 1px 4px -2px rgb(0 0 0 / 13%), 0 2px 8px rgb(0 0 0 / 8%), 0 8px 16px 4px rgb(0 0 0 / 4%);
-}
-.text{
-  width:22px;
-  height:22px;
-  border-radius:22px;
-  display:inline-block;
-}
+
 </style>
