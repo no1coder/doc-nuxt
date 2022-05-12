a6725a465b7e53fa6c4c86597a0509ac2cc5907a
连靖 <1447503844@qq.com>
2022-05-12 11:25:34 +0800

    连靖:5.12

---
+++ components/Catalogue.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <div class="down border-t-2 border-state-300  ">
+    <div class="container mx-auto  flex items-center place-content-center flex-col pt-2">
+      <div class="flex justify-between w-3/5 pt-3">
+        <n-collapse>
+          <template #header-extra>
+            <n-icon><img src="assets/images/jia.svg"  alt=""></n-icon>
+            <n-icon><img src="assets/images/sangedian.svg"  alt=""></n-icon>
+          </template>
+          <template #arrow>
+
+            <n-icon><img src="assets/images/you.svg"  alt=""></n-icon>
+          </template>
+          <n-collapse-item title="青铜" name="1">
+            <n-divider dashed>
+              虚线
+            </n-divider>
+            <div>可以</div>
+          </n-collapse-item>
+          <n-collapse-item title="白银" name="2">
+            <div>很好</div>
+          </n-collapse-item>
+          <n-collapse-item title="黄金" name="3">
+            <div>真棒</div>
+          </n-collapse-item>
+        </n-collapse>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CashOutline as CashIcon } from '@vicons/ionicons5'
+
+export default defineComponent({
+  components: {
+    CashIcon
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/Details.vue
@@ -0,0 +1,35 @@
+<template>
+  <Navigation>
+<!--      <div class="left"></div>-->
+<!--      <div class="right"></div>-->
+  </Navigation>
+<!--  上-->
+  <Center >
+        <div class="one"></div>
+        <div class="two"></div>
+        <div class="three"></div>
+        <div class="four f"></div>
+  </Center>
+<!--  中-->
+  <Catalogue class="down"></Catalogue>
+<!--下-->
+
+
+</template>
+<script lang="ts" setup>
+
+import Navigation from "~/components/Navigation.vue";
+import Center from "~/components/Center.vue";
+import Catalogue from "~/components/Catalogue.vue";
+
+</script>
+
+<style scoped>
+@import "assets/css/tailwind.css";
+img{
+  width: 28px;
+  height: 28px;
+
+}
+
+</style>
