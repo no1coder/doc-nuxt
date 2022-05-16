7e135f5f543e5d60ae9ead89a48b3e56ff12c47d
zhuyuxing <15630620809@163.com>
2022-05-16 13:08:55 +0800

    5.16

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -3,7 +3,7 @@
       <!-- 头部 -->
       <div class="bg-stone-900 box-border border-b border-b-zinc-800 fixed w-full">
         <nav class="container m-auto h-16 flex items-center">
-          <NuxtLink to="/">
+          <NuxtLink to="/" class="hidden sm:block">
             <h1 class="logo h-8"></h1>
           </NuxtLink>
           <input class="outline-0 navbar-input h-9 w-4/12 text-sm px-9 mx-12 rounded-2xl" type="text" placeholder="搜索">
@@ -40,10 +40,7 @@
       <div class="bg-stone-900">
         <div class="container m-auto min-h-screen">
           <CategoriesAnchor :obj="category"/>
-          <CategoriesCard/>
-          <CategoriesCard/>
-          <CategoriesCard/>
-          <CategoriesCard/>
+          <CategoriesCard v-for="items in bookData" :items="items" :key="items.id"/>
         </div>
       </div>
 
@@ -55,6 +52,19 @@
 
 let category = ref({})
 category.value = await f.get(`/api/book_categories`)
+console.log(category.value )
+let bookData = reactive([
+  {"id":79,"name":"\u524d\u7aef","hash_id":"2kK"},
+  {"id":80,"name":"\u751f\u6001","hash_id":"31M"},
+  {"id":81,"name":"\u5927\u6570\u636e","hash_id":"4x1"},
+  {"id":82,"name":"python","hash_id":"5yB"},
+  {"id":83,"name":"Golang","hash_id":"68n"},
+  {"id":386,"name":"Java","hash_id":"924"},
+  {"id":387,"name":"PHP","hash_id":"09G"},
+  {"id":395,"name":"Linux","hash_id":"pzr"},
+  {"id":396,"name":"UI\/UE","hash_id":"qVR"},
+  {"id":399,"name":"\u5176\u4ed6","hash_id":"wEm"}
+])
 
 
 </script>
