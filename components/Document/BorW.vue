86a1916300a22157ad5cb1197837cf921dea2c8f
lin <lin@xue.com>
2022-05-18 17:11:45 +0800

    0518x

--- components/Document/BorW.vue
+++ components/Document/BorW.vue
@@ -8,19 +8,29 @@
 </template>
 
 <script setup>
-
 let isChange = ref(true)
 const change = ()=>{
   isChange.value = !isChange.value
   localStorage.theme = 'dark'
       console.log(localStorage.theme)
+  if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
 }
+onMounted(()=>{
 
-/*if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}*/
+  if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+
+})
+
+
+
 //
 // // Whenever the user explicitly chooses light mode
 // localStorage.theme = 'light'
